77100c6b6b27f129008e93c40a8f25bff7343e77
fraser-chapman <fraser.chapman@autotrader.co.uk>
2019-01-30 17:11:29 +0000

    renamed application runner class and renamed directory folders

--- src/main/java/com/manchesterdigital/hackathonpractice/Application.java
+++ src/main/java/com/manchesterdigital/hackathonpractice/Application.java
@@ -1,13 +1,13 @@
-package com.manhchesterdigital.hackathonpractice;
+package com.manchesterdigital.hackathonpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HackathonpracticeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HackathonpracticeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
