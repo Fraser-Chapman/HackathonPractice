41c8c0f138f95895497b34f576548dec34972766
fraser-chapman <fraser.chapman@autotrader.co.uk>
2019-01-30 16:53:31 +0000

    initial seed of project

---
+++ src/main/java/com/manhchesterdigital/hackathonpractice/HackathonpracticeApplication.java
@@ -0,0 +1,14 @@
+package com.manhchesterdigital.hackathonpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonpracticeApplication.class, args);
+	}
+
+}
+
