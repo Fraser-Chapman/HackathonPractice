62b3c12f8e23b5e9d0290f19960b548c6acdca2b
Fraser <fraserchapman16@gmail.com>
2019-02-02 22:58:35 +0000

    swapped out front end files for semi working fornt end files made on pc. They can call the endpoint but are getting issues due to domain

---
+++ frontend/main.js
@@ -0,0 +1,16 @@
+console.log('Working!');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://127.0.0.1:8080/api/carpark', true);
+xhr.send();
+
+xhr.addEventListener("readystatechange", processRequest, false);
+
+
+function processRequest(e) {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.response);
+        alert(response.carParks);
+
+    }
+}
